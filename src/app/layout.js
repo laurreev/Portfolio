1958a802847a460b5de395260ac0445ba5c30203
exypn <168415500+laurreev@users.noreply.github.com>
2025-07-17 23:58:39 +0800

    Analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 "use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
